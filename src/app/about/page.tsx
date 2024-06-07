2bd1620b68bf55ec29629c018702f9ccf4f32227
dvverma03 <dvverma9211@gmail.com>
2024-06-08 00:36:45 +0530

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import AboutSectionThree from "@/components/About/AboutSectionThree";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "This is About Page",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <AboutSectionThree/>
+      <AboutSectionTwo/>
+    </>
+  );
+};
+
+export default AboutPage;
